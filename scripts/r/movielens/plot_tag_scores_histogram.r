73191963417812608b812a89e27e2c7b4b6d2d04
Martijn de Vos <mdmartijndevos@gmail.com>
2022-01-24 16:46:50 +0100

    Updated MovieLens plot script

--- scripts/r/movielens/plot_tag_scores_histogram.r
+++ scripts/r/movielens/plot_tag_scores_histogram.r
@@ -3,9 +3,10 @@ library(dplyr)
 
 dat <- read.csv("../../../scripts/create_movielens_experiment/data/tag_votes.csv")
 dat$score = dat$upvotes - dat$downvotes
+dat$upvotes <- dat$upvotes - 1  # Remove the upvote of the creator
 dat_filtered <- dat[dat$score > -20,]
 dat_filtered <- dat_filtered[dat_filtered$score <= 40,]
-dat_filtered <- dat_filtered[!(dat_filtered$upvotes == 1 & dat_filtered$downvotes == 0),]
+dat_filtered <- dat_filtered[!(dat_filtered$upvotes == 0 & dat_filtered$downvotes == 0),]
 
 p <- ggplot(dat_filtered, aes(x=score)) +
      geom_histogram(bins=60) +
