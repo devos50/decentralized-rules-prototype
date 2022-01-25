cf76b78d683bcf9732264c839dcbb81ae9072dcb
Martijn de Vos <mdmartijndevos@gmail.com>
2022-01-25 15:54:43 +0100

    Added script to create scenario

---
+++ scripts/create_movielens_experiment/create_scenario.py
@@ -0,0 +1,133 @@
+"""
+Create a scenario for the evaluation of the tagging mechanism.
+"""
+import csv
+import random
+
+import numpy as np
+
+NUM_USERS = 100
+NUM_MOVIES = 10
+TAKE_RANDOM_MOVIES = True  # If false, we will take the top X tagged movies
+DURATION = 300  # Experiment duration in seconds
+MAX_X = 300
+
+# Parameters for the estimation of user tag creation/deletion behaviour
+MU = 2.386474
+SIGMA = 2.107494
+
+# TODO include adversaries
+
+popular_movie_ids = []
+movie_ids_we_have_tags_for = set()
+movies_to_include = []
+min_max_tag_timestamps_per_movie = {}
+experiment_start_time = 1E20
+experiment_end_time = -1
+votes_per_movie = {}
+initial_tag_timestamps = {}
+
+
+def get_user_that_tagged():
+    while True:
+        s = np.random.lognormal(MU, SIGMA, 1)
+        if s[0] > MAX_X:
+            continue
+
+        # Determine the lucky user
+        bin_size = MAX_X / NUM_USERS
+        user_id = int(s[0] / bin_size)
+        return user_id
+
+
+# Get the list of popular movies
+with open("data/tags_per_movie.csv") as in_file:
+    csv_reader = csv.reader(in_file)
+    next(csv_reader)
+    for row in csv_reader:
+        popular_movie_ids.append(row[0])
+
+
+# Read the initial timestamps
+with open("data/initial_tag_timestamp.csv") as in_file:
+    csv_reader = csv.reader(in_file)
+    next(csv_reader)
+    for row in csv_reader:
+        movie_id = row[0]
+        tag = row[1]
+        tag_timestamp = int(row[2])
+
+        if movie_id not in min_max_tag_timestamps_per_movie:
+            min_max_tag_timestamps_per_movie[movie_id] = [1E20, -1]
+        min_max_tag_timestamps_per_movie[movie_id][0] = min(min_max_tag_timestamps_per_movie[movie_id][0], tag_timestamp)
+        min_max_tag_timestamps_per_movie[movie_id][1] = max(min_max_tag_timestamps_per_movie[movie_id][1], tag_timestamp)
+
+        if (movie_id, tag) not in initial_tag_timestamps:
+            initial_tag_timestamps[(movie_id, tag)] = 1E20
+        if tag_timestamp < initial_tag_timestamps[(movie_id, tag)]:
+            initial_tag_timestamps[(movie_id, tag)] = tag_timestamp
+
+# Store the votes on movies
+with open("data/tag_votes.csv") as in_file:
+    csv_reader = csv.reader(in_file)
+    next(csv_reader)
+    for row in csv_reader:
+        movie_id = row[0]
+        movie_ids_we_have_tags_for.add(movie_id)
+        tag = row[1]
+        if movie_id not in votes_per_movie:
+            votes_per_movie[movie_id] = {}
+        votes_per_movie[movie_id][tag] = (int(row[2]), int(row[3]))
+
+# Determine which movies to include
+if TAKE_RANDOM_MOVIES:
+    movies_to_include = random.sample(list(movie_ids_we_have_tags_for), NUM_MOVIES)
+else:
+    movies_to_include = popular_movie_ids[:NUM_MOVIES]
+
+# Determine the experiment start/end times
+for movie_id in movies_to_include:
+    min_time, max_time = min_max_tag_timestamps_per_movie[movie_id]
+    if min_time < experiment_start_time:
+        experiment_start_time = min_time
+    if max_time > experiment_end_time:
+        experiment_end_time = max_time
+
+
+print("Experiment start time: %d" % experiment_start_time)
+print("Experiment end time: %d" % experiment_end_time)
+
+# Create the scenario
+tag_actions = []   # Tuple (timestamp, user_id, movie_id, tag, is_create)
+for movie_id in movies_to_include:
+    for tag, voting_info in votes_per_movie[movie_id].items():
+        num_upvotes, num_downvotes = voting_info
+
+        lowest_timestamp_for_create_tag = 1E20
+        for ind in range(num_upvotes):
+            if ind == 0:  # Initial creation - set it to the initial timestamp if available
+                if (movie_id, tag) in initial_tag_timestamps:
+                    tag_timestamp = initial_tag_timestamps[(movie_id, tag)]
+                else:
+                    # Initial timestamp if not available - it's random
+                    tag_timestamp = random.randint(min_max_tag_timestamps_per_movie[movie_id][0], experiment_end_time)
+                    initial_tag_timestamps[(movie_id, tag)] = tag_timestamp
+            else:
+                tag_timestamp = random.randint(initial_tag_timestamps[(movie_id, tag)], experiment_end_time)
+
+            tag_actions.append((tag_timestamp, get_user_that_tagged(), movie_id, tag, True))
+            if tag_timestamp < lowest_timestamp_for_create_tag:
+                lowest_timestamp_for_create_tag = tag_timestamp
+
+        for ind in range(num_downvotes):
+            tag_timestamp = random.randint(lowest_timestamp_for_create_tag, experiment_end_time)
+            tag_actions.append((tag_timestamp, get_user_that_tagged(), movie_id, tag, False))
+
+tag_actions = sorted(tag_actions, key=lambda t: t[0])  # Sort on timestamp
+
+# Write the scenario
+with open("data/scenarios/tag_experiment_%d.scenario" % NUM_USERS, "w") as scenario_file:
+    for timestamp, user_id, movie_id, tag, is_create in tag_actions:
+        relative_timestamp = timestamp - experiment_start_time
+        scaled_timestamp = relative_timestamp / (experiment_end_time - experiment_start_time) * DURATION
+        scenario_file.write("%f,%d,%s,%s,%d\n" % (scaled_timestamp, user_id, movie_id, tag, 1 if is_create else 0))
--- scripts/create_movielens_experiment/parse_movielens_tags.py
+++ scripts/create_movielens_experiment/parse_movielens_tags.py
@@ -10,6 +10,7 @@ TAGS_FILE = "/Users/martijndevos/Downloads/ml-25m/tags.csv"
 tags_per_user = defaultdict(lambda: 0)
 tags_per_movie = defaultdict(lambda: 0)
 total_votes = 0
+first_tag_timestamp = {}
 
 with open(TAGS_FILE) as tags_file:
     parsed_header = False
@@ -18,9 +19,15 @@ with open(TAGS_FILE) as tags_file:
 
     for csv_line in csv_reader:
         user_id, movie_id, tag, timestamp = csv_line
+        timestamp = int(timestamp)
         tags_per_user[user_id] += 1
         total_votes += 1
         tags_per_movie[movie_id] += 1
+
+        if (movie_id, tag) not in first_tag_timestamp:
+            first_tag_timestamp[(movie_id, tag)] = timestamp
+        elif first_tag_timestamp[(movie_id, tag)] > timestamp:
+            first_tag_timestamp[(movie_id, tag)] = timestamp
 
 # Write data
 print("Writing data...")
@@ -33,6 +40,11 @@ with open("data/tags_per_movie.csv", "w") as out_file:
     out_file.write("movie_id,num_tags\n")
     for movie_id, num_tags in sorted(tags_per_movie.items(), key=lambda x: x[1], reverse=True):
         out_file.write("%s,%s\n" % (movie_id, num_tags))
+
+with open("data/initial_tag_timestamp.csv", "w") as out_file:
+    out_file.write("movie_id,first_tag_timestamp\n")
+    for movie_id, tag in first_tag_timestamp.keys():
+        out_file.write("%s,%s,%d\n" % (movie_id, tag.replace(",", ""), first_tag_timestamp[(movie_id, tag)]))
 
 print("Total votes: %d" % total_votes)
 print("Unique users that tagged: %d" % len(tags_per_user))
